734aad29c1af9e05a5e9f96f1d184f8ffc4b3f40
griffFu <gfulto.9@gmail.com>
2022-05-26 13:02:06 -0500

    created a round and game to play for the user, can not handle errors for the moment

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
+// giving the computer a selection between the three options
 function computerplay(){
-    let options = ["Rock","Paper","Scissors"]
+    let options = ["rock","paper","scissors"]
     let valueToUse = options[Math.floor(Math.random() * options.length)]
     return valueToUse
 }
 
-
+// function that prompts player to chose their option for rock paper scissors
 function playerselection(){
     let selection = prompt("What do you choose?")
     newSelection = selection.toLowerCase()
@@ -13,14 +14,50 @@ function playerselection(){
 }
 
 
-playerselection()
-
-function playround(computerplay, newSelection){
 
 
+// function that plays a round and tells the user what 
+function playround(computerplay, playerselection){
+    if (computerplay == playerselection){
+        console.log("You both chose " + computerplay + ". You tie!")
+    }
+
+    else if (computerplay != playerselection){
+        if (playerselection === "rock" && computerplay === "scissors"){
+            console.log("The computer chose " +  computerplay + ". You win!")
+        }
+
+        if (playerselection === "rock" && computerplay === "paper"){
+            console.log("The computer chose " +  computerplay + ". You lost!")
+        }
+
+        if (playerselection === "scissors" && computerplay === "rock"){
+            console.log("The computer chose " +  computerplay + ". You lost!")
+        }
+
+        if (playerselection === "scissors" && computerplay === "paper"){
+            console.log("The computer chose " +  computerplay + ". You win!")
+        }
+        
+        if (playerselection === "paper" && computerplay === "scissors"){
+            console.log("The computer chose " +  computerplay + ". You lost!")
+        }
+
+        if (playerselection === "paper" && computerplay === "rock"){
+            console.log("The computer chose " +  computerplay + ". You lost!")
+        }
+        
+    }
 
 }
-let computer = computerplay
 
-//hey griff, so as of now you need to create a round of play where the comparison is made between the two variables
-//then, you can create the game where the user will play 5 rounds. Also, maybe keep a tally. 
+function playgame(){
+    for (let i = 0; i<5; i++){
+        let comp = computerplay()
+        let player = playerselection()
+        console.log(playround(comp,player))
+    }
+}
+
+playgame()
+
